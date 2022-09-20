31d946f806cf088796913a243ec4edccfee952ee
zhuzzzhha <90785698+zhuzzzhha@users.noreply.github.com>
2022-09-20 09:03:26 +0300

    Update PriorityQueue.cpp

--- PriorityQueue.cpp
+++ PriorityQueue.cpp
@@ -5,15 +5,16 @@
 #include <memory>
 #include <vector>
 #include <algorithm>
+template <typename T>
 struct Queue {
 	struct Node {
-		int data;
+		T data;
 		std::unique_ptr <Node> next;
 		Node():
 			data(0),
 			next(nullptr)
 		{}
-		Node(int value):
+		Node(T value):
 			data(value),
 			next(nullptr)
 		{}
@@ -24,13 +25,23 @@ struct Queue {
 		while (head)
 			head = std::move(head->next);
 	}
-	void push(int value)
+	Queue() = default;
+	Queue(const std::vector<T>& v)
+	{
+		
+		for (int i = 0; i < v.size(); i++)
+		{
+			push(v[i]);
+		}
+	}
+	Queue(const Queue& copy) = default;
+	void push(T value)
 	{
 		Node* ind = head.get();
 		Node* prev = head.get();
 		if (value > ind->data)
 			ind = (ind->next).get();
-		while (value > ind->data)
+		while (value > ind->data && ind->next!=nullptr)
 		{
 			ind=(ind->next).get();
 			prev = (prev->next).get();
@@ -45,8 +56,12 @@ struct Queue {
 	{
 		head = std::move(head->next);
 	}
-	int top()
+	T top()
 	{
 		return head->data;
 	}
 };
+int main()
+{
+	return 0;
+}
